f6b97291990fda2dd3b4d98b213b818119051e10
Edahi Garrido <edahiegs@gmail.com>
2022-09-20 17:48:02 -0500

    Actualizacion del Fetch

--- src/Layout/pages/API.jsx
+++ src/Layout/pages/API.jsx
@@ -8,7 +8,7 @@ const API = () => {
   const getGifs = async () => {
     const api_key = "q4ztbSSk5kcxdgLDkT6dgLRwOI10akh7";
     const category = "Valorant";
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=15`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=15`;
 
     const resp = await fetch(url);
     const { data } = await resp.json();
